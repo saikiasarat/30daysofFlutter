e06e99db36370c2bb0e18f026f762155c8704248
Sarat Saikia <saratsaikia91@gmail.com>
2021-07-27 00:08:05 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
 
-import 'home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.teal),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
